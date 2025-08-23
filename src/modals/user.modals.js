f8dcb28d8390d829f09ded62fd408f7153f47141
anant613 <anantkapoor320@gmail.com>
2025-08-23 17:17:01 +0530

    database

--- src/modals/user.modals.js
+++ src/modals/user.modals.js
@@ -3,7 +3,45 @@ const { Schema } = require("mongoose");
 import mongoose , {Schema} from "mongoose";
 
 const user_schema = new Schema({
-    
+    username : {
+        type : String,
+        unique : true,
+        require : true,
+        lowerase : true,
+        index : true
+    },
+    email : {
+        type : String,
+        unique : true,
+        require : true,
+        lowerase : true
+    },
+    Fullname : {
+        type : String,
+        require : true,
+        lowerase : true,
+        index : true
+    },
+    avatar :{
+        type : String,
+        require : true
+    },
+    CoverImage :{
+        type : String
+    },
+    watchHistory :{
+        type: Schema.Types.ObjectId,
+        ref : "Video"
+    },
+    password:{
+        type:String,
+        required : [true,'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{
+    timestamps:true
 })
 
 export const User = mongoose.model("User" ,  user_schema)
---
+++ src/modals/video.modals.js
@@ -0,0 +1,39 @@
+import { Timestamp } from "bson";
+import mongoose , {Schema, SchemaType} from "mongoose";
+import { types } from "util";
+
+const Video_schema = new Schema({
+    Videofile : {
+        type : String,
+        require : true
+    },
+    thumbnail:{
+        type:String , 
+        require: true
+    },
+    title:{
+        type:String , 
+        require: true
+    },
+    views:{
+        type: Number , 
+        default:0
+    },
+    description:{
+        type : String,
+        require : true,
+    },
+    duration:{
+        type:Number,
+        require:true
+    },
+    isPublished:{
+        type: Boolean , 
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+export const Video = mongoose.model("Video" , Video_schema)
